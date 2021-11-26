dcccc6bbd35fda6d9168c305daa258986b3caa79
Karim Ajour <karim.ajour@hva.nl>
2021-11-26 17:04:45 +0100

    Initial commit

---
+++ src/main/java/com/example/project_bigbangk/ProjectBigBangKApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_bigbangk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBigBangKApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBigBangKApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_bigbangk/ProjectBigBangKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_bigbangk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBigBangKApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
