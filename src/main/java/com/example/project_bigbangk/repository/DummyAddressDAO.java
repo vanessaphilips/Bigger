cbe9c8c3c5821c07fb20da6c5e161b777a3e422d
Ray <ray.schimmel@hva.nl>
2021-12-02 13:35:28 +0100

    adresstraat aanpassingen

---
+++ src/main/java/com/example/project_bigbangk/repository/DummyAddressDAO.java
@@ -0,0 +1,17 @@
+// Created by RayS
+// Creation date 2-12-2021
+
+package com.example.project_bigbangk.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DummyAddressDAO {
+
+    private final Logger logger = LoggerFactory.getLogger(DummyAddressDAO.class);
+
+    public DummyAddressDAO() {
+        super();
+        logger.info("New AddressDAO");
+    }
+}
