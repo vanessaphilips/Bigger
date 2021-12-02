9b33c48b37885f92c0f0120d5f091a58c16f79e6
Vanessa Philips <vanessa.philips@hva.nl>
2021-12-02 13:36:52 +0100

    Client DAO interface

--- src/main/java/com/example/project_bigbangk/repository/IClientDAO.java
+++ src/main/java/com/example/project_bigbangk/repository/IClientDAO.java
@@ -1,5 +1,19 @@
 package com.example.project_bigbangk.repository;
 
+import com.example.project_bigbangk.model.Client;
+
+import java.util.List;
+
 public interface IClientDAO {
+
+    public void saveClient(Client mpClient);
+
+    public Client findClientByEmail(int email);
+
+    public List<Client> findAllClients();
+
+    public void updateClient(Client client);
+
+    public List<Client> findClientByLastName(String lastName);
 
 }
