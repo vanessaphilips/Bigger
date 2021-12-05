7b1cc636e3ca68202cd03ec87d4bd59031272a26
Philip Beeltje <philip.beeltje@hva.nl>
2021-12-05 23:54:40 +0100

    registration toegevoegd, met service en datatransfer object (json voorbeeld in comment van de service)

---
+++ src/main/java/com/example/project_bigbangk/controller/Registration.java
@@ -0,0 +1,32 @@
+package com.example.project_bigbangk.controller;
+/*
+
+@Author Philip Beeltje, Studentnummer: 500519452
+*/
+
+import com.example.project_bigbangk.model.Client;
+import com.example.project_bigbangk.model.RegistrationDTO;
+import com.example.project_bigbangk.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/client")
+public class Registration {
+
+    private RegistrationService registrationService;
+
+
+    public Registration(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping("/register")
+    @ResponseBody
+    public void receiveRegistrationInput( @RequestBody RegistrationDTO registrationDTO) {
+        //krijgt een error als de inkomende data niet voldoet aan het object, wat als verkeerde data toch door frontend komt?
+        registrationService.registerClient(registrationDTO);
+    }
+
+}
