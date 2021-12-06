6eb12b9be062533dacb9a35710c45de456de2409
Karim Ajour <karim.ajour@hva.nl>
2021-12-06 14:06:27 +0100

    WalletDAO updates en find querys aangemaakt

--- src/main/java/com/example/project_bigbangk/repository/IWalletDAO.java
+++ src/main/java/com/example/project_bigbangk/repository/IWalletDAO.java
@@ -1,8 +1,15 @@
 package com.example.project_bigbangk.repository;
 
+import com.example.project_bigbangk.model.Wallet;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public interface IWalletDAO {
 
+public void saveWallet(Wallet wallet);
 
+public void updateBalance(Wallet wallet);
+
+public void findWalletByIban(String iban);
+
+public void findAllWallets();
 }
--- src/main/java/com/example/project_bigbangk/repository/WalletDAO.java
+++ src/main/java/com/example/project_bigbangk/repository/WalletDAO.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class WalletDAO {
@@ -19,7 +20,25 @@ public class WalletDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    
+    public void saveWallet(Wallet wallet) {
+        String slq = "Insert into wallet values(?,?);";
+        jdbcTemplate.update(slq, wallet.getIban(), wallet.getBalance());
+    }
+
+    public void updateBalance(Wallet wallet) {
+        String slq = "Update wallet Set balance = ?";
+        jdbcTemplate.update(slq, wallet.getBalance());
+    }
+
+    public Wallet findWalletByIban(String iban) {
+        String slq = "Select * From wallet Where IBAN = ?;";
+        return jdbcTemplate.queryForObject(slq, new walletRowMapper(), iban);
+    }
+
+    public List<Wallet> findAllWallets() {
+        String slq = "Select * From wallet;";
+        return jdbcTemplate.query(slq, new walletRowMapper());
+    }
 
     public class walletRowMapper implements RowMapper<Wallet> {
 
