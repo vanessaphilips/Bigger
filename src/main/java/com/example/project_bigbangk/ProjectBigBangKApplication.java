34e75e3455605eb4d1dcd14e8cd7dab1c496b25c
Philip Beeltje <philip.beeltje@hva.nl>
2021-12-06 16:18:53 +0100

    fix voor security error

--- src/main/java/com/example/project_bigbangk/ProjectBigBangKApplication.java
+++ src/main/java/com/example/project_bigbangk/ProjectBigBangKApplication.java
@@ -2,8 +2,12 @@ package com.example.project_bigbangk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class ProjectBigBangKApplication {
 
     public static void main(String[] args) {
@@ -11,3 +15,5 @@ public class ProjectBigBangKApplication {
     }
 
 }
+
+
