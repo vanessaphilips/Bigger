8e1de6db167f0642a6c45bd1efaa4b6b8c06e9cf
Pieter Jan Bleichrodt <deek.bleichrodt@hva.nl>
2021-12-09 14:24:16 +0100

    LoginService en LoginServiceTest maken nu gebruik van de rootrepository.findCLientbyEmail i.p.v. clientService.getClientByEmail

--- src/main/java/com/example/project_bigbangk/service/LoginService.java
+++ src/main/java/com/example/project_bigbangk/service/LoginService.java
@@ -4,6 +4,7 @@
 package com.example.project_bigbangk.service;
 
 import com.example.project_bigbangk.model.Client;
+import com.example.project_bigbangk.repository.RootRepository;
 import com.example.project_bigbangk.service.Security.AuthenticateService;
 import com.example.project_bigbangk.service.Security.ITokenService;
 import org.slf4j.Logger;
@@ -18,22 +19,22 @@ import org.springframework.stereotype.Service;
 public class LoginService {
     private final String TOKEN_PREFIX = "Bearer ";
     private final Logger logger = LoggerFactory.getLogger(LoginService.class);
-    ClientService clientService;
+    RootRepository rootRepository;
     AuthenticateService authenticateService;
     ITokenService jwtService;
 
     @Autowired
-    public LoginService(AuthenticateService authenticateService, ITokenService jwtService, ClientService clientService) {
+    public LoginService(AuthenticateService authenticateService, ITokenService jwtService, RootRepository rootRepository) {
         super();
         logger.info("New LoginService");
-        this.clientService = clientService;
+        this.rootRepository = rootRepository;
         this.authenticateService = authenticateService;
         this.jwtService = jwtService;
     }
 
     public String login(String email, String password) {
         if ((email != null && password != null && authenticateService.authenticate(email, password))) {
-            Client client = clientService.getClientByEmail(email);
+            Client client = rootRepository.findClientByEmail(email);
             String token = jwtService.getToken(email, client.getFirstName());
             logger.info(String.format("Login user %s succesfull", client.getEmail()));
             return TOKEN_PREFIX + token;
--- src/test/java/com/example/project_bigbangk/service/LoginServiceTest.java
+++ src/test/java/com/example/project_bigbangk/service/LoginServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.project_bigbangk.service;
 
 import com.example.project_bigbangk.model.Client;
+import com.example.project_bigbangk.repository.RootRepository;
 import com.example.project_bigbangk.service.Security.AuthenticateService;
 import com.example.project_bigbangk.service.Security.ITokenService;
 import com.example.project_bigbangk.service.Security.JWTService;
@@ -16,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class LoginServiceTest {
 
     static ITokenService tokenService = Mockito.mock(JWTService.class);
-    static ClientService clientService = Mockito.mock(ClientService.class);
+    static RootRepository rootRepository = Mockito.mock(RootRepository.class);
     static AuthenticateService authenticateService = Mockito.mock(AuthenticateService.class);
     static Client client = Mockito.mock(Client.class);
     static LoginService loginService;
@@ -26,15 +27,15 @@ class LoginServiceTest {
     @BeforeAll
     public static void setup() {       Mockito.when(client.getFirstName()).thenReturn("Deek");
         Mockito.when(client.getEmail()).thenReturn("deek@deek.nl");
-        Mockito.when(clientService.getClientByEmail(EMAIL)).thenReturn(client);
-        Mockito.when(tokenService.getToken(EMAIL, client.getFirstName())).thenReturn("succes");
-        loginService = new LoginService(authenticateService, tokenService, clientService);
+        Mockito.when(rootRepository.findClientByEmail(EMAIL)).thenReturn(client);
+        Mockito.when(tokenService.getToken(EMAIL, client.getFirstName())).thenReturn("token");
+        loginService = new LoginService(authenticateService, tokenService, rootRepository);
     }
 
     @Test
     public void loginSucces() {
         Mockito.when(authenticateService.authenticate(EMAIL, PASSWORD)).thenReturn(true);
-        String expected = "succes";
+        String expected = "Bearer token";
         String actual = loginService.login("deek@deek.nl", "DeekPW");
         assertEquals(expected, actual);
     }
