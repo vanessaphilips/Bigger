345560c7924622f845c91fd0dcad85e8691426d2
Pieter Jan Bleichrodt <deek.bleichrodt@hva.nl>
2021-12-09 14:58:12 +0100

    Rootrepository merge conflict 'hopelijk' opgelost

--- src/main/java/com/example/project_bigbangk/repository/RootRepository.java
+++ src/main/java/com/example/project_bigbangk/repository/RootRepository.java
@@ -7,6 +7,7 @@
 
 package com.example.project_bigbangk.repository;
 
+import com.example.project_bigbangk.model.Asset;
 import com.example.project_bigbangk.model.Client;
 import com.example.project_bigbangk.model.Address;
 import com.example.project_bigbangk.model.Wallet;
@@ -15,52 +16,75 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class RootRepository {
 
-   private IClientDAO clientDAO;
-   private IAddressDAO addressDAO;
-   private IWalletDAO walletDAO;
+    private IClientDAO clientDAO;
+    private IAddressDAO addressDAO;
+    private IWalletDAO walletDAO;
 
-   public RootRepository(IClientDAO clientDAO, IAddressDAO addressDAO, IWalletDAO walletDAO) {
-      this.clientDAO = clientDAO;
-      this.addressDAO = addressDAO;
-      this.walletDAO = walletDAO;
-   }
+    public RootRepository(IClientDAO clientDAO, IAddressDAO addressDAO, IWalletDAO walletDAO) {
+        this.clientDAO = clientDAO;
+        this.addressDAO = addressDAO;
+        this.walletDAO = walletDAO;
+    }
 
-   // CLIENT
+    // CLIENT
 
-   public Client findClientByEmail(String email) {
-      Client client = clientDAO.findClientByEmail(email);
-      if (client == null) {
-      }
-      return client;
-   }
-
-
-
-   public Address findAddressByEmail(String email) {
-      Address address = addressDAO.findAddressByEmail(email);
-      if (address == null) {
-      }
-      return address;
-   }
-
-   /**
-    * Saves address, wallet and client seperately in Database.
-    * @param client
-    */
-   public void createNewlyRegisteredClient(Client client){
-      addressDAO.saveAddress(client.getAddress());
-      walletDAO.createNewWallet(client.getWallet());
-      clientDAO.saveClient(client);
-      //FIXME moet nog hier of in addressDAO opvangen wat er gebeurt als een address er al in staat, client check ik al in registratieservice
-      //
+    public Client findClientByEmail(String email) {
+        Client client = clientDAO.findClientByEmail(email);
+        if (client == null) {
+        }
+        return client;
+    }
 
 
-   }
+    public Address findAddressByEmail(String email) {
+        Address address = addressDAO.findAddressByEmail(email);
+        if (address == null) {
+        }
+        return address;
+    }
+
+    /**
+     * Saves address, wallet and client seperately in Database.
+     *
+     * @param client
+     */
+    public void createNewlyRegisteredClient(Client client) {
+        addressDAO.saveAddress(client.getAddress());
+        walletDAO.createNewWallet(client.getWallet());
+        clientDAO.saveClient(client);
+    }
 
 
-   // IBAN
+    // IBAN
 
-   public Wallet findWalletByIban(String iban) {
-      return walletDAO.findWalletByIban(iban);
-   }
+    public Wallet findWalletByIban(String iban) {
+        return walletDAO.findWalletByIban(iban);
+    }
+
+    // WALLET
+
+    //TODO methode invullen
+    public void createNewWalletWithAssets() {
+    }
+
+    public void updateWalletBalanceAndAsset(Wallet wallet, Asset asset) {
+        walletDAO.updateWalletBalanceAndAsset(wallet, asset);
+    }
+
+    //TODO methode invullen
+//   public Wallet findWalletWithAssetByIban(String iban) {
+//      Wallet wallet = walletDAO.findWalletByIban(iban);
+//      if (wallet == null) {
+//         return wallet;
+//      }
+//      Map<String, Double> assetMap = walletDAO.findAssetCodeWithAmount(iban);
+//      Map<Asset, Double> returnAssetMap = null;
+//      for ( String asset : assetMap.keySet()) {
+    //TODO AssetDOA functies aanmaken
+//         returnAssetMap.put(assetDOA.findAssetByCode(asset),assetMap.get(asset));
+//      }
+//      wallet.setAsset(returnAssetMap);
+//
+//      return wallet;
+//   }
 }
