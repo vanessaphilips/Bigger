fed80bc011a248e02af46e3fa5fea7e3c62ed1a0
Philip Beeltje <philip.beeltje@hva.nl>
2021-12-14 15:33:05 +0100

    registration basics, alle velden zijn er en de submit knop vult een js klasse. json.stringify maakt er een jason van

--- src/main/resources/static/scripts/RegistrationScript.js
+++ src/main/resources/static/scripts/RegistrationScript.js
@@ -1,14 +1,38 @@
 
-function getFormData($form){
-    let unindexed_array = $form.serializeArray();
-    let indexed_array = {};
-
-    $.map(unindexed_array, function(n, i){
-        indexed_array[n['id']] = n['value'];
-    });
-
-    return indexed_array;
+class RegistrationDTO{
+    constructor(email,password, firstName, insertion, lastName,
+    bsn, dateOfBirth, postalCode,street, number, city, country)
+{
+    this.email = email;
+    this.password = password;
+    this.firstName = firstName;
+    this.insertion = insertion;
+    this.lastName = lastName;
+    this.bsn = bsn;
+    this.dateOfBirth = dateOfBirth;
+    this.postalCode = postalCode;
+    this.street = street;
+    this.number = number;
+    this.city = city;
+    this.country = country;
+    }
 }
 
-let $form = $("#form_data");
-let data = getFormData($form);
+function saveRegistration() {
+    let registration = new RegistrationDTO(
+        document.getElementById('email').value,
+        document.getElementById('password').value,
+        document.getElementById('firstName').value,
+        document.getElementById('insertion').value,
+        document.getElementById('lastName').value,
+        document.getElementById('bsn').value,
+        document.getElementById('dateOfBirth').value,
+        document.getElementById('postalCode').value,
+        document.getElementById('street').value,
+        parseInt(document.getElementById('number').value),
+        document.getElementById('city').value,
+        document.getElementById('county').value
+    );
+    alert(JSON.stringify(registration));
+}
+
