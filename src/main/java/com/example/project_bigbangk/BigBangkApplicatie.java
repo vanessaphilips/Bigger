923ff055da9f7f0f0b714a70f58321af2c352aea
Pieter Jan Bleichrodt <deek.bleichrodt@hva.nl>
2021-12-15 20:23:04 +0100

    ClientFactory toegevoed. Werkt maar is nog even disabled

--- src/main/java/com/example/project_bigbangk/BigBangkApplicatie.java
+++ src/main/java/com/example/project_bigbangk/BigBangkApplicatie.java
@@ -3,6 +3,8 @@
 
 package com.example.project_bigbangk;
 
+import com.example.project_bigbangk.repository.RootRepository;
+import com.example.project_bigbangk.service.ClientFactory;
 import com.example.project_bigbangk.service.priceHistoryUpdate.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,19 +23,33 @@ public class BigBangkApplicatie implements ApplicationListener<ContextRefreshedE
     //ToDO Misschien nog een Singleton bank object hier instantieren? Zodat de bank er altijd is.
     private final PriceHistoryUpdateService priceHistoryUpdateService;
     private final ICryptoApiNegotiatorStrategy cryptoApiNegotiatorStrategy;
+    private ClientFactory clientFactory;
+    private final int NUMBER_OF_CLIENTS_TO_SEED = 3000;
+    private RootRepository rootRepository;
 
 
-    public BigBangkApplicatie(PriceHistoryUpdateService priceHistoryUpdateService, ICryptoApiNegotiatorStrategy cryptoApiNegotiatorStrategy) {
+    public BigBangkApplicatie(PriceHistoryUpdateService priceHistoryUpdateService,
+                              ICryptoApiNegotiatorStrategy cryptoApiNegotiatorStrategy,
+                              ClientFactory clientFactory,
+    RootRepository rootrepository) {
         super();
         logger.info("New BigBangkApplicatie");
         this.priceHistoryUpdateService = priceHistoryUpdateService;
         this.cryptoApiNegotiatorStrategy = cryptoApiNegotiatorStrategy;
+        this.clientFactory = clientFactory;
+        this.rootRepository = rootrepository;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         initializeUpdateStragegy();
         startPriceHistoryUpdateTimer();
+      //  seedDataBase();
+    }
+
+    private void seedDataBase() {
+        //TODO checken of er al wat in de DB staat
+        clientFactory.createClients(NUMBER_OF_CLIENTS_TO_SEED);
     }
 
     private void initializeUpdateStragegy() {
---
+++ src/test/java/com/example/project_bigbangk/service/ClientFactoryTest.java
@@ -0,0 +1,32 @@
+package com.example.project_bigbangk.service;
+
+import com.example.project_bigbangk.repository.RootRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientFactoryTest {
+
+    @Test
+    void createCLients() {
+    }
+
+//    @Test
+//    void initializeLastNamesAndInsertions() {
+//        ClientFactory clientFactory = new ClientFactory());
+//        Map<String, String> lastNames = clientFactory.initializeLastNames();
+//        System.out.println(lastNames.size());
+//        assertTrue(lastNames.size()>9000);
+//    }
+//    @Test
+//    void initializeFistNames() {
+//        ClientFactory clientFactory = new ClientFactory();
+//       List<String> firstNames = clientFactory.initializeFirstNames();;
+//        System.out.println(firstNames.size());
+//        assertTrue(firstNames.size()>9000);
+//    }
+
+}
