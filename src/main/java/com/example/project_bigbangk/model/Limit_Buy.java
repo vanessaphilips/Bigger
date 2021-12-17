a4950d05331342a289122dc06893a8457ffe193a
Vanessa Philips <vanessa.philips@hva.nl>
2021-12-17 14:30:21 +0100

    order/transaction verder uitgewerkt

--- src/main/java/com/example/project_bigbangk/model/Limit_Buy.java
+++ src/main/java/com/example/project_bigbangk/model/Limit_Buy.java
@@ -3,6 +3,30 @@
 
 package com.example.project_bigbangk.model;
 
-public class Limit_Buy {
+import java.time.LocalDateTime;
 
+public class Limit_Buy extends AbstractOrder{
+
+    private Wallet buyerWallet;
+
+    public Limit_Buy(int orderId, Asset asset, double requestedPrice, int numberOfAssets,
+                     LocalDateTime date, double transactionFee, Wallet buyerWallet) {
+        super(orderId, asset, requestedPrice, numberOfAssets, date, transactionFee);
+        this.buyerWallet = buyerWallet;
+    }
+
+    public Wallet getBuyerWallet() {
+        return buyerWallet;
+    }
+
+    public void setBuyerWallet(Wallet buyerWallet) {
+        this.buyerWallet = buyerWallet;
+    }
+
+    @Override
+    public String toString() {
+        return "Limit_Buy{" +
+                "buyerWallet=" + buyerWallet +
+                '}';
+    }
 }
