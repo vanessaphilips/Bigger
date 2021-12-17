6ad3d04592d5e0a7085fdf454d863505a5ac015a
Karim Ajour <karim.ajour@hva.nl>
2021-12-17 14:40:23 +0100

    JdbcWalletDAOTest aangemaakt

---
+++ src/test/java/com/example/project_bigbangk/repository/JdbcWalletDAOTest.java
@@ -0,0 +1,28 @@
+package com.example.project_bigbangk.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JdbcWalletDAOTest {
+
+    @Test
+    void saveNewWallet() {
+    }
+
+    @Test
+    void updateBalance() {
+    }
+
+    @Test
+    void findWalletByIban() {
+    }
+
+    @Test
+    void updateWalletAssets() {
+    }
+
+    @Test
+    void findAmountOfAsset() {
+    }
+}
