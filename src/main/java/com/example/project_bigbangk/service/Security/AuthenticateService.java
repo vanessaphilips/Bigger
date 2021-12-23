6561bed8f97130e2250d8e005247e68ba32d1449
Pieter Jan Bleichrodt <deek.bleichrodt@hva.nl>
2021-12-23 11:30:48 +0100

    public Client getClientFromToken(String token) aangemaakt in AuthenticateService. Verder commentaar toegevoegd boven alle public methods

--- src/main/java/com/example/project_bigbangk/service/Security/AuthenticateService.java
+++ src/main/java/com/example/project_bigbangk/service/Security/AuthenticateService.java
@@ -24,7 +24,11 @@ public class AuthenticateService {
         this.tokenService = tokenService;
         this.hashService = hashService;
     }
-
+    /**
+     * Authenticate the login credentials which you got from the clientside in an request
+     * @param email
+     * @return true if the combination is valid
+     */
     public boolean authenticate(String email, String password) {
         Client client = rootRepository.findClientByEmail(email);
         if (client != null) {
@@ -33,8 +37,27 @@ public class AuthenticateService {
         return false;
     }
 
+    /**
+     * Authenticate your token which you got from the clientside in an request
+     * @param token the token you  get from the client
+     * @return true if the token is valid
+     */
     public boolean authenticate(String token) {
-        token = token.replace("Bearer ", "");
-        return tokenService.authenticateToken(token);
+        return tokenService.authenticateToken(removeBearer(token));
+    }
+
+    /**
+     * Get the Client which belongs to the request you get from clientside by sending the corresponding token
+     * @param token the token you  get from the client
+     * @return
+     */
+    public Client getClientFromToken(String token){
+        String email =   tokenService.getEmailFromToken(removeBearer(token));
+        Client client = rootRepository.findClientByEmail(email);
+        return client;
+    }
+
+    private String removeBearer(String token){
+       return token.replace("Bearer ", "");
     }
 }
