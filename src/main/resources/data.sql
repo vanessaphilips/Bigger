6d5cd6a4a3eaacc06fbef8254bc784cf4f8789e2
Pieter Jan Bleichrodt <deek.bleichrodt@hva.nl>
2021-12-24 10:39:39 +0100

    insert script PriceHistory voor h2 database gemaakt. test toegevoegd. Werkt nog niet

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,34 @@
-INSERT INTO `Client` VALUES
-                            ('michael@oosterhout.nl', 'Michael',NULL, 'Oosterhout', '1965-10-10', '123456789', 'michaeloosterhout', null, null, null),
-                            ('gerke@deboer.nl', 'Gerke','de', 'Boer', '1959-03-02', '123456789', 'gerkedeboer', null, null, null),
-                            ('huub@vanthienen.nl','Huub','van', 'Thienen', '1965-11-11', '123456789', 'huubvanthienen', null, null, null),
-                            ('michel@oey.nl', 'Michel', NULL, 'Oey', '1968-09-09', '123456789', 'micheloey', null, null, null),
-                            ('sander@deboer.nl', 'Sander', 'de', 'Boer', '1966-09-09', '123456789', 'sanderdeboer', null, null, null),
-                            ('nicole@wong.nl', 'Nicole', NULL, 'Wong', '1973-01-01', '123456789', 'nicolewong', null, null, null);
+INSERT INTO `Client`
+VALUES ('michael@oosterhout.nl', 'Michael', NULL, 'Oosterhout', '1965-10-10', '123456789', 'michaeloosterhout', null,
+        null, null),
+       ('gerke@deboer.nl', 'Gerke', 'de', 'Boer', '1959-03-02', '123456789', 'gerkedeboer', null, null, null),
+       ('huub@vanthienen.nl', 'Huub', 'van', 'Thienen', '1965-11-11', '123456789', 'huubvanthienen', null, null, null),
+       ('michel@oey.nl', 'Michel', NULL, 'Oey', '1968-09-09', '123456789', 'micheloey', null, null, null),
+       ('sander@deboer.nl', 'Sander', 'de', 'Boer', '1966-09-09', '123456789', 'sanderdeboer', null, null, null),
+       ('nicole@wong.nl', 'Nicole', NULL, 'Wong', '1973-01-01', '123456789', 'nicolewong', null, null, null);
 
-INSERT INTO `bank` VALUES
-  ('BGBK', 'Big Bangk', 'NL17 BGBK 7265511', 1000.00, 5.0);
+INSERT INTO `bank`
+VALUES ('BGBK', 'Big Bangk', 'NL17 BGBK 7265511', 1000.00, 5.0);
 
-INSERT INTO `Wallet` VALUES
-                            ('NL20BGBK0001234567', 10000.00);
+INSERT INTO `Wallet`
+VALUES ('NL20BGBK0001234567', 10000.00);
 
-INSERT INTO `wallet_has_asset` VALUES
-                            ('BTC', 10, 'NL20BGBK0001234567'),
-                            ('ADA', 10, 'NL20BGBK0001234567'),
-                            ('ETH', 10, 'NL20BGBK0001234567'),
-                            ('DOGE', 10, 'NL20BGBK0001234567'),
-                            ('LUNA', 10, 'NL20BGBK0001234567');
+INSERT INTO `wallet_has_asset`
+VALUES ('BTC', 10, 'NL20BGBK0001234567'),
+       ('ADA', 10, 'NL20BGBK0001234567'),
+       ('ETH', 10, 'NL20BGBK0001234567'),
+       ('DOGE', 10, 'NL20BGBK0001234567'),
+       ('LUNA', 10, 'NL20BGBK0001234567');
+
+INSERT INTO `PriceHistory`
+VALUES ('2005-12-31 21:00:59Z', 'BTC', 44000),
+       ('2005-12-31 21:05:59Z', 'BTC', 44200),
+       ('2005-12-31 21:10:59Z', 'BTC', 44370),
+       ('2005-12-31 21:15:59Z', 'BTC', 44880),
+       ('2005-12-31 21:20:59Z', 'BTC', 44332),
+       ('2005-12-31 21:00:59Z', 'ETH', 2.1),
+       ('2005-12-31 21:05:59Z', 'ETH', 2.6),
+       ('2005-12-31 21:10:59Z', 'ETH', 2.7),
+       ('2005-12-31 21:15:59Z', 'ETH', 2.0),
+       ('2005-12-31 21:20:59Z', 'ETH', 2.5);
+
--- src/test/java/com/example/project_bigbangk/repository/JdbcPriceHistoryDAOTest.java
+++ src/test/java/com/example/project_bigbangk/repository/JdbcPriceHistoryDAOTest.java
@@ -1,5 +1,6 @@
 package com.example.project_bigbangk.repository;
 
+import com.example.project_bigbangk.BigBangkApplicatie;
 import com.example.project_bigbangk.model.Asset;
 import com.example.project_bigbangk.model.AssetCode_Name;
 import com.example.project_bigbangk.model.PriceHistory;
@@ -7,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.annotation.Resource;
@@ -14,11 +17,13 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class JdbcPriceHistoryDAOTest {
+
 
     @Resource
     JdbcPriceHistoryDAO priceHistoryDAO;
@@ -29,20 +34,24 @@ class JdbcPriceHistoryDAOTest {
 
     @BeforeEach
     public void setup() {
-        priceHistories.add(createPriceHisory(0.7, createAsset(AssetCode_Name.BTC)));
-        priceHistories.add(createPriceHisory(1.3, createAsset(AssetCode_Name.BTC)));
-        priceHistories.add(createPriceHisory(1.1, createAsset(AssetCode_Name.BTC)));
-        priceHistories.add(createPriceHisory(1.21, createAsset(AssetCode_Name.BTC)));
-        priceHistories.add(createPriceHisory(1.3, createAsset(AssetCode_Name.ETH)));
-        priceHistories.add(createPriceHisory(4.3, createAsset(AssetCode_Name.ETH)));
-        priceHistories.add(createPriceHisory(2.3, createAsset(AssetCode_Name.ETH)));
-        priceHistories.add(createPriceHisory(6.3, createAsset(AssetCode_Name.ETH)));
-        priceHistories.add(createPriceHisory(1.5, createAsset(AssetCode_Name.ADA)));
-        priceHistories.add(createPriceHisory(5.5, createAsset(AssetCode_Name.ADA)));
-        priceHistories.add(createPriceHisory(2.5, createAsset(AssetCode_Name.ADA)));
-        priceHistories.add(createPriceHisory(0.5, createAsset(AssetCode_Name.ADA)));
+        priceHistories.add(createPriceHistory(0.7, createAsset(AssetCode_Name.BTC)));
+        priceHistories.add(createPriceHistory(1.3, createAsset(AssetCode_Name.BTC)));
+        priceHistories.add(createPriceHistory(1.1, createAsset(AssetCode_Name.BTC)));
+        priceHistories.add(createPriceHistory(1.21, createAsset(AssetCode_Name.BTC)));
+        priceHistories.add(createPriceHistory(1.3, createAsset(AssetCode_Name.ETH)));
+        priceHistories.add(createPriceHistory(4.3, createAsset(AssetCode_Name.ETH)));
+        priceHistories.add(createPriceHistory(2.3, createAsset(AssetCode_Name.ETH)));
+        priceHistories.add(createPriceHistory(6.3, createAsset(AssetCode_Name.ETH)));
+        priceHistories.add(createPriceHistory(1.5, createAsset(AssetCode_Name.ADA)));
+        priceHistories.add(createPriceHistory(5.5, createAsset(AssetCode_Name.ADA)));
+        priceHistories.add(createPriceHistory(2.5, createAsset(AssetCode_Name.ADA)));
+        priceHistories.add(createPriceHistory(0.5, createAsset(AssetCode_Name.ADA)));
     }
-
+@Test
+void getAllPriceHistory(){
+      List<PriceHistory> actual = priceHistoryDAO.getPriceHistoriesByCodeFromDate(LocalDateTime.now().minusSeconds(15), "BTC");
+      assertThat(actual.size()).isEqualTo(4);
+}
 
     @Test
     void savePriceHistory() {
@@ -58,7 +67,8 @@ class JdbcPriceHistoryDAOTest {
     }
 
 
-    private PriceHistory createPriceHisory(double currentprice, Asset asset) {
+
+    private PriceHistory createPriceHistory(double currentprice, Asset asset) {
         PriceHistory priceHistory = Mockito.mock(PriceHistory.class);
         Mockito.when(priceHistory.getPrice()).thenReturn(currentprice);
         Mockito.when(priceHistory.getDateTime()).thenReturn(LocalDateTime.now());
