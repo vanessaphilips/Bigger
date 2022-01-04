834bc8e1a166c41d66c172f99dc63400160b7802
Pieter Jan Bleichrodt <deek.bleichrodt@hva.nl>
2022-01-04 16:29:47 +0100

    graphjs en googlegraph refactored

--- src/main/resources/static/scripts/GoogleGraph.js
+++ src/main/resources/static/scripts/GoogleGraph.js
@@ -1,6 +1,25 @@
+function setData(data, dates, prices) {
+    data.addColumn('date', 'date');
+    data.addColumn('number', 'price');
+    for (let date of dates) {
+        const row = []
+        row.push(date)
+        row.push(prices[dates.indexOf(date)])
+        data.addRow(row)
+    }
+}
+
+function setOptions() {
+    return {
+        title: {display: false},
+        curveType: 'function',
+        legend: {display: false},
+        height: 100,
+        width: 200,
+    };
+}
+
 const createGraph = (priceHistoriesOfAsset) => {
-    //  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    //  <script type="text/javascript">
     let dates = priceHistoriesOfAsset.map(ph => ph.date)
     let prices = priceHistoriesOfAsset.map(ph => ph.price)
     const divGraph = document.createElement("div")
@@ -9,31 +28,12 @@ const createGraph = (priceHistoriesOfAsset) => {
 
     function drawChart() {
         const data = new google.visualization.DataTable()
-        //console.log(dates)
-        data.addColumn('date', 'date');
-        data.addColumn('number', 'price');
-        for (let date of dates) {
-            const row = []
-            row.push(date)
-            row.push(prices[dates.indexOf(date)])
-            data.addRow(row)
-        }
+        setData(data, dates, prices)
         console.log("laden data googleGraph gelukt")
-
-        var options = {
-            title: {display: false},
-            curveType: 'function',
-            legend: {display: false},
-            height: 100,
-            width: 200,
-
-        };
-
+        var options = setOptions()
         const chart = new google.visualization.LineChart(divGraph);
         chart.draw(data, options);
 
     }
-
     return divGraph
-
 }
--- src/main/resources/static/scripts/Graphjs.js
+++ src/main/resources/static/scripts/Graphjs.js
@@ -1,25 +1,29 @@
-const createGraph = ( priceHistoriesOfAsset) => {
+function setData(dates, prices) {
+    return {
+        labels: dates,
+        datasets: [{
+            legend: {display: false},
+            pointBackgroundColor: "white",
+            backgroundColor: "white",
+            borderColor: "rgba(0,0,0,0.5)",
+            data: prices,
+            fill: false
+        }]
+    }
+}
+
+const createGraph = (priceHistoriesOfAsset) => {
     let dates = priceHistoriesOfAsset.map(ph => ph.date)
     let prices = priceHistoriesOfAsset.map(ph => ph.price)
     const graphJs = document.createElement("canvas")
     new Chart(graphJs, {
         type: "line",
-        data: {
-            labels: dates,
-            datasets: [{
-                legend: {display: false},
-                pointBackgroundColor: "white",
-                backgroundColor: "white",
-                borderColor: "rgba(0,0,0,0.5)",
-                data: prices,
-                fill: false
-            }]
-        },
+        data: setData(dates, prices),
         options: {
             spanGaps: true,
             datasets: {
                 line: {
-                    pointRadius: 0 // disable for all `'line'` datasets
+                    pointRadius: 1 // disable for all `'line'` datasets
                 }
             }
         }
