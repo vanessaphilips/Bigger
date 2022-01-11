b9a1d10446c30ff1f77a3f87f056457d6735865f
Pieter Jan Bleichrodt <deek.bleichrodt@hva.nl>
2022-01-11 15:04:43 +0100

    DummyLogin registreerd nu eerst de user zodat login altijd goed gaat.

--- src/main/resources/static/scripts/DummyLogin.js
+++ src/main/resources/static/scripts/DummyLogin.js
@@ -5,9 +5,58 @@ class LoginDTO {
     }
 }
 
-const loginDTO = new LoginDTO("henk@unicom.nl", "password1234345")
+class RegistrationDTO {
+    constructor(email, password, firstName, insertion, lastName,
+                bsn, dateOfBirth, postalCode, street, number, city, country) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.insertion = insertion;
+        this.lastName = lastName;
+        this.bsn = bsn;
+        this.dateOfBirth = dateOfBirth;
+        this.postalCode = postalCode;
+        this.street = street;
+        this.number = number;
+        this.city = city;
+        this.country = country;
+    }
+}
+
+const loginDTO = new LoginDTO("henk@uncom.nl", "password1234345")
+
+async function register() {
+    let registration = new RegistrationDTO(
+        "henk@uncom.nl",
+        "password1234345",
+        "Henk",
+        "de",
+        "Kort",
+        "123434546",
+        "1950-01-01",
+        "1111BN",
+        "Straatie",
+        9,
+        "Muiden",
+        "NLD"
+    );
+    await fetch(`${rootURL}register`, {
+        method: "POST",
+        headers: acceptHeaders(),
+        body: JSON.stringify(registration)
+    }).then(response => {
+        if (response.status === 201) {
+            console.log("Succesfull registration user" + registration)
+        } else if (response.status === 409) {
+            console.log("User already in database" + registration.email)
+        } else {
+            console.log("Bad registration" + response.body)
+        }
+    })
+}
 
 async function getToken() {
+    await register()
     await fetch(`${rootURL}login`, {
         method: "POST",
         headers: acceptHeaders(),
@@ -20,7 +69,6 @@ async function getToken() {
         }).then((json) => {
             if (json.authorization !== undefined) {
                 localStorage.setItem("jwtToken", json.authorization)
-                console.log("dummy lofin " + json.authorization + Date.now())
                 console.log("login succes" + loginDTO.email);
             } else {
                 console.log("login failed");
