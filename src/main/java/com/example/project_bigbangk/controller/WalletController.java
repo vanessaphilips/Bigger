bb4941c382b6a16f340912bd624676c52211e6b5
Kelly Speelman <kelly.speelman-de.jonge@hva.nl>
2022-01-12 16:24:14 +0100

    WalletService opnieuw van begin af aangemaakt. Hier wordt nu alleen gekeken voor de portefeuille pagina. En de WalletController is aangemaakt.

---
+++ src/main/java/com/example/project_bigbangk/controller/WalletController.java
@@ -0,0 +1,47 @@
+package com.example.project_bigbangk.controller;
+
+import com.example.project_bigbangk.model.Wallet;
+import com.example.project_bigbangk.service.Security.AuthenticateService;
+import com.example.project_bigbangk.service.WalletService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Author Kelly Speelman - de Jonge
+ */
+
+@CrossOrigin
+@RestController
+public class WalletController {
+    private final Logger logger = LoggerFactory.getLogger(WalletController.class);
+    private final AuthenticateService authenticateService;
+    private final WalletService walletService;
+    private final ObjectMapper MAPPER = new ObjectMapper();
+
+    @Autowired
+    public WalletController(AuthenticateService authenticateService, WalletService walletService) {
+        super();
+        this.authenticateService = authenticateService;
+        this.walletService = walletService;
+    }
+
+    @PostMapping("/wallet")
+    @ResponseBody
+    public ResponseEntity<String> gotoWalletScreen(@RequestHeader String authorization){
+        if (authenticateService.authenticate(authorization)){
+            try {
+                Wallet wallet = walletService.getWalletClient(authorization);
+                String jsonWallet = MAPPER.writeValueAsString(wallet);
+                return ResponseEntity.ok().body(jsonWallet);
+            } catch (JsonProcessingException exception) {
+                logger.error(exception.getMessage());
+            }
+        }
+        return ResponseEntity.status(401).body("token expired");
+    }
+}
--- src/main/java/com/example/project_bigbangk/service/WalletService.java
+++ src/main/java/com/example/project_bigbangk/service/WalletService.java
@@ -1,35 +1,27 @@
 package com.example.project_bigbangk.service;
 
-import com.example.project_bigbangk.model.Asset;
+import com.example.project_bigbangk.model.Client;
 import com.example.project_bigbangk.model.Wallet;
-import com.example.project_bigbangk.repository.JdbcWalletDAO;
-import com.example.project_bigbangk.repository.RootRepository;
+import com.example.project_bigbangk.service.Security.AuthenticateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+/**
+ * @Author Kelly Speelman - de Jonge
+ */
 
 @Service
 public class WalletService {
+    private final AuthenticateService authenticateService;
 
-    private final int START_CAPITAL_NEW_USER = 10000;
-
-    private RootRepository rootRepository;
-    private IbanGeneratorService ibanGeneratorService;
-
-    public WalletService(JdbcWalletDAO jdbcWalletDAO, RootRepository rootRepository, IbanGeneratorService ibanGeneratorService) {
-        this.rootRepository = rootRepository;
-        this.ibanGeneratorService = ibanGeneratorService;
+    @Autowired
+    public WalletService (AuthenticateService authenticateService) {
+        super();
+        this.authenticateService = authenticateService;
     }
 
-    public void saveNewWallet() {
-        Wallet wallet = new Wallet(ibanGeneratorService.getIban(), START_CAPITAL_NEW_USER);
-        rootRepository.saveNewWallet(wallet);
+    public Wallet getWalletClient(String token){
+        Client client = authenticateService.getClientFromToken(token);
+        return client.getWallet();
     }
-
-    public void updateWalletBalanceAndAsset(Wallet wallet, Asset asset, double amount) {
-        rootRepository.updateWalletBalanceAndAsset(wallet, asset, amount);
-    }
-
-    public Wallet findWalletWithAssetByIban(String iban){
-        return rootRepository.findWalletWithAssetByIban(iban);
-    }
-
 }
