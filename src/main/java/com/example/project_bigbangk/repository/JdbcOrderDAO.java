ed453624e9ac260ec91d32f89e43db4adb12e39d
Pieter Jan Bleichrodt <deek.bleichrodt@hva.nl>
2022-01-19 16:10:52 +0100

    Methodes getALl voor LimitBuy LimitSell en StopLoss in orderDAO en RootRepository gemaakt. deleteOrderById gemaakt orderId in AbstractOrder aangepast naar int ipv long

--- src/main/java/com/example/project_bigbangk/repository/JdbcOrderDAO.java
+++ src/main/java/com/example/project_bigbangk/repository/JdbcOrderDAO.java
@@ -8,8 +8,10 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Created by Vanessa Philips.
@@ -30,7 +32,7 @@ public class JdbcOrderDAO {
     }
 
     //Transaction
-    public void saveTransaction(Transaction transaction){
+    public void saveTransaction(Transaction transaction) {
         String sql = "INSERT INTO bigbangk.order (buyer, seller, assetCode, orderType, assetAmount, date, fee, priceExcludingFee) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
 
         try {
@@ -38,7 +40,7 @@ public class JdbcOrderDAO {
                     transaction.getBuyerWallet().getIban(),
                     transaction.getSellerWallet().getIban(),
                     transaction.getAsset().getCode(),
-                    TransactionType.TRANSACTION,
+                    TransactionType.TRANSACTION.toString(),
                     transaction.getAssetAmount(),
                     java.sql.Timestamp.valueOf(transaction.getDate()),
                     transaction.getFee(),
@@ -49,7 +51,7 @@ public class JdbcOrderDAO {
         }
     }
 
-    public Transaction findTransactionById(Long orderId){
+    public Transaction findTransactionById(Long orderId) {
         String sql = "SELECT * FROM Order WHERE id=?;";
         try {
             return jdbcTemplate.queryForObject(sql, new TransactionRowMapper(), orderId);
@@ -59,61 +61,12 @@ public class JdbcOrderDAO {
         return null;
     }
 
-    private static class TransactionRowMapper implements RowMapper<Transaction> {
-
-        @Override
-        public Transaction mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-            int orderId = resultSet.getInt("orderId");
-            Double priceExcludingFee = resultSet.getDouble("priceExcludingFee");
-            Integer assetAmount = resultSet.getInt("assetAmount");
-            LocalDateTime date = resultSet.getObject("date", LocalDateTime.class);
-            Double fee = resultSet.getDouble("fee");
-            return new Transaction(orderId, priceExcludingFee, assetAmount, date, fee);
-        }
-    }
-    private static class LimitSellRowMapper implements RowMapper<Limit_Sell> {
-
-        @Override
-        public Limit_Sell mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-            int orderId = resultSet.getInt("orderId");
-            Double requestedPrice = resultSet.getDouble("orderLimit");
-            Integer assetAmount = resultSet.getInt("assetAmount");
-            LocalDateTime date = resultSet.getObject("date", LocalDateTime.class);
-            return new Limit_Sell(orderId, requestedPrice, assetAmount, date);
-        }
-    }
-
-    private static class LimitBuyRowMapper implements RowMapper<Limit_Buy> {
-
-        @Override
-        public Limit_Buy mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-            int orderId = resultSet.getInt("orderId");
-            Double limit = resultSet.getDouble("orderLimit");
-            Integer assetAmount = resultSet.getInt("assetAmount");
-            LocalDateTime date = resultSet.getObject("date", LocalDateTime.class);
-            return new Limit_Buy(orderId, limit, assetAmount, date);
-        }
-    }
-    private static class StopLossRowMapper implements RowMapper<Stoploss_Sell> {
-
-        @Override
-        public Stoploss_Sell mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-            int orderId = resultSet.getInt("orderId");
-            Double limit = resultSet.getDouble("orderLimit");
-            Integer assetAmount = resultSet.getInt("assetAmount");
-            LocalDateTime date = resultSet.getObject("date", LocalDateTime.class);
-
-            return new Stoploss_Sell(orderId,  limit, assetAmount, LocalDateTime.now());
-        }
-    }
-
-
     /**
      * Saves Limit_Buy order in database, waiting to be matched with another client's offer (matchservice).
-     * @param limit_buy
-     * author = Vanessa Philips
+     *
+     * @param limit_buy author = Vanessa Philips
      */
-    public void saveLimit_Buy(Limit_Buy limit_buy){
+    public void saveLimit_Buy(Limit_Buy limit_buy) {
         String sql = "INSERT INTO bigbangk.order (buyer, assetCode, orderType, orderlimit, assetAmount, date) VALUES (?, ?, ?, ?, ?, ?);";
 
         try {
@@ -132,10 +85,10 @@ public class JdbcOrderDAO {
 
     /**
      * Saves Limit_Sell order in database, waiting to be matched with another client's offer (matchservice).
-     * @param limit_sell
-     * author = Vanessa Philips
+     *
+     * @param limit_sell author = Vanessa Philips
      */
-    public void saveLimit_Sell(Limit_Sell limit_sell){
+    public void saveLimit_Sell(Limit_Sell limit_sell) {
         String sql = "INSERT INTO bigbangk.order (seller, assetCode, orderType, orderlimit, assetAmount, date) VALUES (?, ?, ?, ?, ?, ?);";
 
         try {
@@ -151,6 +104,97 @@ public class JdbcOrderDAO {
         }
     }
 
+    public List<Limit_Buy> getAllLimitBuys() {
+        String sql = "SELECT * FROM bigbangk.Order WHERE orderType=?;";
+        try {
+            return jdbcTemplate.query(sql, new LimitBuyRowMapper(), TransactionType.LIMIT_BUY.toString());
+        } catch (DataAccessException dataAccessException) {
+            logger.info(dataAccessException.getMessage());
+        }
+        return null;
+    }
 
+    public List<Limit_Sell> getAllLimitSells() {
+        String sql = "SELECT * FROM bigbangk.Order WHERE orderType=?;";
+        try {
+            return jdbcTemplate.query(sql, new LimitSellRowMapper(), TransactionType.LIMIT_SELL.toString());
+        } catch (DataAccessException dataAccessException) {
+            System.err.println(dataAccessException.getMessage());
+        }
+        return null;
+    }
+
+    public List<Stoploss_Sell> getAllStopLossSells() {
+        String sql = "SELECT * FROM bigbangk.Order WHERE orderType=?;";
+        try {
+            return jdbcTemplate.query(sql, new StopLossRowMapper(), TransactionType.STOPLOSS_SELL.toString());
+        } catch (DataAccessException dataAccessException) {
+            System.err.println(dataAccessException.getMessage());
+        }
+        return null;
+    }
+
+    public boolean deleteOrderById(int orderId){
+        String sql = "DELETE FROM bigbangk.Order where orderID=?;";
+        try{
+            return jdbcTemplate.update(sql, orderId)>0;
+        } catch (DataAccessException dataAccessException) {
+            System.err.println(dataAccessException.getMessage());
+        }
+        return false;
+    }
+
+    private static class TransactionRowMapper implements RowMapper<Transaction> {
+
+        @Override
+        public Transaction mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            int orderId = resultSet.getInt("orderId");
+            double priceExcludingFee = resultSet.getDouble("priceExcludingFee");
+            double assetAmount = resultSet.getDouble("assetAmount");
+            LocalDateTime date = resultSet.getObject("date", LocalDateTime.class);
+            double fee = resultSet.getDouble("fee");
+            return new Transaction(orderId, priceExcludingFee, assetAmount, date, fee);
+        }
+    }
+
+    private static class LimitSellRowMapper implements RowMapper<Limit_Sell> {
+
+        @Override
+        public Limit_Sell mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            return createOrderOfType(resultSet, TransactionType.LIMIT_SELL);
+        }
+    }
+
+    private static class LimitBuyRowMapper implements RowMapper<Limit_Buy> {
+
+        @Override
+        public Limit_Buy mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            return createOrderOfType(resultSet, TransactionType.LIMIT_BUY);
+        }
+    }
+
+    private static class StopLossRowMapper implements RowMapper<Stoploss_Sell> {
+
+        @Override
+        public Stoploss_Sell mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            return createOrderOfType(resultSet, TransactionType.STOPLOSS_SELL);
+        }
+    }
+
+    static private <T> T createOrderOfType(ResultSet resultSet, TransactionType type) throws SQLException {
+        int orderId = resultSet.getInt("orderId");
+        double limit = resultSet.getDouble("orderLimit");
+        double assetAmount = resultSet.getDouble("assetAmount");
+        LocalDateTime date = resultSet.getObject("date", LocalDateTime.class);
+        switch (type) {
+            case LIMIT_BUY:
+                return (T) new Limit_Buy(orderId, limit, assetAmount, date);
+            case LIMIT_SELL:
+                return (T) new Limit_Sell(orderId, limit, assetAmount, date);
+            case STOPLOSS_SELL:
+                return (T) new Stoploss_Sell(orderId, limit, assetAmount, date);
+        }
+        return null;
+    }
 
 }
--- src/test/java/com/example/project_bigbangk/service/OrderMatchingServiceTest.java
+++ src/test/java/com/example/project_bigbangk/service/OrderMatchingServiceTest.java
@@ -31,20 +31,21 @@ class OrderMatchingServiceTest {
         //Asset asset, double requestedPrice, double numberOfAssets, LocalDateTime date, Wallet sellerWallet
         Asset asset = rootRepository.getAllAssets().stream()
                 .filter(a -> a.getCode().equals(AssetCode_Name.BTC.getAssetCode())).findFirst().orElse(null);
-        Wallet wallet1 = rootRepository.findWalletByEmail("0@Samsung.de");
-        Wallet wallet2 = rootRepository.findWalletByEmail("0@Samsung.fr");
-        Wallet wallet3 = rootRepository.findWalletByEmail("A@Yahoo.de");
-        Wallet wallet4 = rootRepository.findWalletByEmail("Aafke@Chello.be");
-        Wallet wallet5 = rootRepository.findWalletByEmail("Aaldert@Yahoo.com");
-        Wallet wallet6 = rootRepository.findWalletByEmail("Aagtje@Chello.org");
-        Wallet wallet7 = rootRepository.findWalletByEmail("Aagtje@T-Mobile.uk");
-        Wallet wallet8 = rootRepository.findWalletByEmail("Aaldrik@work.nl");
+        Wallet wallet1 = rootRepository.findWalletByEmail("0@Yahoo.com");
+        Wallet wallet2 = rootRepository.findWalletByEmail("Aalt@T-Mobile.biz");
+        Wallet wallet3 = rootRepository.findWalletByEmail("Aartie@Gmail.de");
+        Wallet wallet4 = rootRepository.findWalletByEmail("Abdelkarim@Wanadoo.de");
+        Wallet wallet5 = rootRepository.findWalletByEmail("Abdelwahed@T-Mobile.be");
+        Wallet wallet6 = rootRepository.findWalletByEmail("Abdeslam@T-Mobile.de");
+        Wallet wallet7 = rootRepository.findWalletByEmail("Abe@Wanadoo.com");
+        Wallet wallet8 = rootRepository.findWalletByEmail("Adriaantje@T-Mobile.nl");
         rootRepository.saveWaitingLimitSellOrder(new Limit_Sell(asset, 5, 2, LocalDateTime.now().minusDays(10), wallet1));
         rootRepository.saveWaitingLimitSellOrder(new Limit_Sell(asset, 4, 2, LocalDateTime.now().minusDays(20), wallet2));
         rootRepository.saveWaitingLimitSellOrder(new Limit_Sell(asset, 3, 2, LocalDateTime.now().minusDays(20), wallet3));
         rootRepository.saveWaitingLimitSellOrder(new Limit_Sell(asset, 5, 2, LocalDateTime.now().minusDays(90), wallet4));
         rootRepository.saveWaitingLimitSellOrder(new Limit_Sell(asset, 3, 2, LocalDateTime.now().minusDays(30), wallet5));
         rootRepository.saveWaitingLimitBuyOrder(new Limit_Buy(asset, 3, 3, LocalDateTime.now().minusDays(30), wallet6));
+        System.out.println(rootRepository.getAllLimitBuy());
         //rootRepository.saveWaitingLimitBuyOrder(new Limit_Buy(asset, 4, 2, LocalDateTime.now().minusDays(45), wallet7));
       //  rootRepository.saveWaitingLimitBuyOrder(new Limit_Buy(asset, 5, 2, LocalDateTime.now().minusDays(50), wallet8));
     }
